da3f87208623802cfe0f18f5bbed99ed431123c0
Sergey Chernyshov <thesaren117@yandex.ru>
2020-08-20 17:06:05 +0300

    Используем args и kwargs

--- main.py
+++ main.py
@@ -3,30 +3,30 @@ import datetime
 
 
 
-def print_function():
-    print('Привет')
+
 
 def decorator(function_to_decorate):
-    def wrapper():
+
+    def wrapper(*args, **kwargs):
+        print('Начало работы программы:', datetime.datetime.now())
         start = datetime.datetime.now()
-        function_to_decorate()
-        log_string = f'Дата и время запуска программы: {start}  \nНазвание программы: {function_to_decorate.__name__}'
+        function_to_decorate(*args, **kwargs)
+        log_string = f'Дата и время запуска программы: {start}  \nНазвание программы: {function_to_decorate.__name__} \nАргументы: {args} {kwargs}'
         with open('log.txt', 'w', encoding='UTF-8') as file:
             file.write(log_string)
         print(log_string)
-    return wrapper()
+    return wrapper
+
+
+@decorator
+def say(name, surname, age):
+    print('Привет', name, surname, age)
 
 
 
 
+say(input("Введите имя:"),input('Введите фамилию:'), input('Введите возраст:'))
 
 
 
-
-
-
-
-
-if __name__ == '__main__':
-    decorator(print_function)
 
